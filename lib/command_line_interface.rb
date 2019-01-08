1b522d6db39d71803e8ea84d9a9bb37abefde490
YSongYS <songyishu.yang@gmail.com>
2019-01-08 15:35:16 +0000

    song version

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
+  puts "Welcome!"
   # puts out a welcome message here!
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  character_name = gets.chomp.split(" ").map(&:capitalize).join(" ")
   # use gets to capture the user's input. This method should return that input, downcased.
 end
