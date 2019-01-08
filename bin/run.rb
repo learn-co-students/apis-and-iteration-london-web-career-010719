434c347e95568cf759429dd1226874f27b61e9aa
YannickHoegerle <yannick.hoegerle@gmail.com>
2019-01-08 17:42:23 +0000

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,17 @@
 #!/usr/bin/env ruby
-
-require_relative "../lib/api_communicator.rb"
+require 'pry'
 require_relative "../lib/command_line_interface.rb"
+require_relative "../lib/api_communicator.rb"
 
+
+def run
 welcome
 character = get_character_from_user
-show_character_movies(character)
+character_info = get_character_data(character)
+
+movies_list = get_character_movies_from_api(character_info)
+movies_info_hash = get_film_info_for_film(movies_list)
+print_movies(movies_info_hash)
+end
+
+run
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,30 +2,63 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
+def get_character_data(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
+  response_string = RestClient.get("http://www.swapi.co/api/people/?search=#{character_name}")
+  JSON.parse(response_string.body)
+end
 
+def get_character_movies_from_api(character_hash)
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
+  return character_hash["results"][0]["films"]
+end
+
+
+
+def get_film_info_for_film(film_links)
+    # collect those film API urls, make a web request to each URL to get the info
+    #  for that film
+  all_films = []
+  film_links.each do |link|
+  response_string = RestClient.get link
+  all_films << JSON.parse(response_string)
+  end
+  all_films
+end
+# return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
+  # get_film_info_for_film(film_links) this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
 
-def print_movies(films)
+def print_movies(movies_info_hash)
   # some iteration magic and puts out the movies in a nice list
-end
+   movies_info_hash.each do |movie|
+    movie.each do |attributes, info|
 
-def show_character_movies(character)
-  films = get_character_movies_from_api(character)
-  print_movies(films)
-end
+      case attributes
+       when "title"
+         puts "-------------------------------"
+         puts "Title: #{info}"
+       when "episode_id"
+         puts "Episode: #{info}"
+       when "director"
+         puts "Directed By: #{info}"
+       when "producer"
+         puts "Produced By: #{info}"
+       when "release_date"
+         puts "Released: #{info}"
+       else
+       end
+     end
+    end
+  end
+
+# def show_character_movies(character)
+#   films = get_character_movies_from_api(character)
+#   print_movies(films)
+# end
 
 ## BONUS
 
