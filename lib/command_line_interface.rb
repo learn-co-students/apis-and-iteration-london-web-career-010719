17682afd9b3896f4eea6f20df788b7d756594530
github username <github email address>
2019-01-08 17:33:01 +0000

    DONE

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -9,13 +9,6 @@ end
 
 def get_character_from_user
   puts "please enter a character name"
-  input = gets.chomp
+  gets.chomp
   # use gets to capture the user's input. This method should return that input, downcased.
 end
-
-def run
-  input = get_character_from_user
-  show_character_movies(input)
-end
-
-run
