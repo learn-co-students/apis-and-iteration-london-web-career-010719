aa25bacf503d6e0f8f0455d3fb27ab468847716f
Chris Camacho <cacamacho91@gmail.com>
2019-01-12 01:38:52 +0000

    test

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,14 @@
+require 'require_relative'
+require_relative "api_communicator.rb"
+
+
+
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to the StarWars App!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  gets.chomp
   # use gets to capture the user's input. This method should return that input, downcased.
 end
